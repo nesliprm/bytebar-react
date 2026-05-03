2cebacf7bf2fdfcb39063223815c56e6ade07c6c
Nesli <nesliparma@gmail.com>
2026-05-03 21:50:12 +0200

    font install

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,15 @@
 export const Header = () => {
   return (
-    <div className="text-center text-2xl font-extrabold mt-10">
-      hello bytebar
-    </div>
+    <>
+      <div className="text-center text-2xl font-extrabold mt-10 font-lobster">
+        hello bytebar
+      </div>
+      <div className="text-center text-2xl font-extrabold mt-10 font-roboto-slab">
+        hello bytebar
+      </div>
+      <div className="text-center text-2xl font-extrabold mt-10 font-tiny5">
+        hello bytebar
+      </div>
+    </>
   );
 };
