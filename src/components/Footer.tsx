46f4f371ba23a7f800cd57e42310f420302f8b9e
Nesli <nesliparma@gmail.com>
2026-05-03 21:43:40 +0200

    reinstall TW

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="fixed left-0 bottom-0 text-center text-xs mb-2 w-full">
-      Coded by ▼ Nesli Parmaksizoglu
+      built by ▼ Nesli Parmaksizoglu
     </div>
   );
 };
