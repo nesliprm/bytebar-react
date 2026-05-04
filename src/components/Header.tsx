fe1da2ea69f939c796f2a1535ce477ba78abccfc
Nesli <nesliparma@gmail.com>
2026-05-04 20:59:11 +0200

    styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 export const Header = () => {
   return (
-    <>
-      <div className="text-center text-2xl font-extrabold mt-10 font-lobster">
-        hello bytebar
+    <div className="bg-teal p-6 rounded-2xl w-full max-w-200 mb-8 mx-auto">
+      <h1 className="font-header font-bold text-7xl text-yellow text-shadow-header">
+        byteBAR
+      </h1>
+      <div className="mt-8 text-sm">
+        <p className="font-main font-bold">
+          Welcome to byteBAR — a virtual mixologist that can craft cocktails!
+        </p>
+        <p>
+          Type an ingredient to search the collection, let our boozy bot whip
+          something up with AI Search, or click Random for a surprise creation!
+        </p>
       </div>
-      <div className="text-center text-2xl font-extrabold mt-10 font-roboto-slab">
-        hello bytebar
-      </div>
-      <div className="text-center text-2xl font-extrabold mt-10 font-tiny5">
-        hello bytebar
-      </div>
-    </>
+    </div>
   );
 };
